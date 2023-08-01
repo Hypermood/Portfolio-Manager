b3b0e1b91b7771558146c664bafbd20cb1ec30d5
Vladimir Pavlov <vladimir.vladimirov.pavlov@gmail.com>
2023-08-01 14:43:49 +0300

    Modified the exception handling to return an array of strings instead of a single string with \n, when dealing with bad requests.

--- src/main/java/com/melon/portfoliomanager/controllers/UserController.java
+++ src/main/java/com/melon/portfoliomanager/controllers/UserController.java
@@ -9,10 +9,15 @@ import com.melon.portfoliomanager.models.User;
 import com.melon.portfoliomanager.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
 
 
 @RestController
@@ -60,14 +65,16 @@ public class UserController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<List<String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         StringBuilder errors = new StringBuilder();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String errorMessage = error.getDefaultMessage();
-            errors.append(errorMessage).append("\n");
-        });
 
-        return new ResponseEntity<>(errors.toString(), HttpStatus.BAD_REQUEST);
+        List<String> errorsMessages = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .filter(Objects::nonNull)
+                .toList();
+
+        return new ResponseEntity<>(errorsMessages, HttpStatus.BAD_REQUEST);
     }
 
 }
